cb2b6593f48fa4312d65d8e810062c749b25a6bf
Lam Dao <lam.dao@edu.sait.ca>
2025-07-23 19:12:48 -0600

    Upload MyArrayList

--- src/implementations/MyArrayList.java
+++ src/implementations/MyArrayList.java
@@ -3,90 +3,185 @@ package implementations;
 import utilities.ListADT;
 import utilities.Iterator;
 
-public class MyArrayList<E> implements ListADT<E> {
+public class MyArrayList<E> implements ListADT<E>, Iterator<E> {
 
+    private E[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = (E[]) new Object[10000];
+        size = 0;
+    }
+    
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return size;
     }
 
     @Override
     public void clear() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (toAdd == null) {
+            throw new NullPointerException("Cannot add null element");
+        }
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = toAdd;
+        size++;
+        return true;
     }
 
     @Override
     public boolean add(E toAdd) throws NullPointerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (toAdd == null) {
+            throw new NullPointerException("Cannot add null element");
+        }
+        elements[size] = toAdd;
+        size++;
+        return true;
     }
 
     @Override
     public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addAll'");
+        if (toAdd == null) {
+            throw new NullPointerException("Cannot add null list");
+        }
+        for (int i = 0; i < toAdd.size(); i++) {
+            add(toAdd.get(i));
+        }
+        return true;
     }
 
     @Override
     public E get(int index) throws IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return elements[index];
     }
 
     @Override
     public E remove(int index) throws IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        E removedElement = elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null; // Clear the last element
+        size--;
+        return removedElement;
     }
 
     @Override
     public E remove(E toRemove) throws NullPointerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        if (toRemove == null) {
+            throw new NullPointerException("Cannot remove null element");
+        }
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(toRemove)) {
+                return remove(i);
+            }
+        }
+        return null;
     }
 
     @Override
     public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'set'");
+        if (toChange == null) {
+            throw new NullPointerException("Cannot set null element");
+        }
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        E oldElement = elements[index];
+        elements[index] = toChange;
+        return oldElement;
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return size == 0;
     }
 
     @Override
     public boolean contains(E toFind) throws NullPointerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+        if (toFind == null) {
+            throw new NullPointerException("Cannot search for null element");
+        }
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(toFind)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public E[] toArray(E[] toHold) throws NullPointerException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+        if (toHold == null) {
+            throw new NullPointerException("Cannot convert to null array");
+        }
+        if (toHold.length < size) {
+            throw new IllegalArgumentException("Provided array is too small");
+        }
+        for (int i = 0; i < size; i++) {
+            toHold[i] = elements[i];
+        }
+        return toHold;
     }
 
     @Override
     public Object[] toArray() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+        Object[] array = new Object[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = elements[i];
+        }
+        return array;
     }
 
     @Override
     public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No more elements");
+                }
+                return elements[currentIndex++];
+            }
+        };
+    }
+
+    @Override
+    public boolean hasNext() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+    }
+
+    @Override
+    public E next() throws NoSuchElementException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
     }
     
 }
